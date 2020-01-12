fe6d5c9edc25621d134c9ad5985761b9ea852f91
Lorenzo <rossi.26.mario@gmail.com>
2020-01-12 16:09:06 +0100

    Updates

---
+++ precache-manifest.84973ffc656e27eeaee575f75b891549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5aae3821c6f28672bba7fa47a79330",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "a73e939ed1cb95de4171",
+    "url": "/client/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9e47b5c718fe1c2d9196",
+    "url": "/client/static/js/2.fef33bc8.chunk.js"
+  },
+  {
+    "revision": "9715fe930583001697de12e64bbbeb79",
+    "url": "/client/static/js/2.fef33bc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a73e939ed1cb95de4171",
+    "url": "/client/static/js/main.f269e663.chunk.js"
+  },
+  {
+    "revision": "9f85c3223e5f02bcffe1",
+    "url": "/client/static/js/runtime-main.ed6e9a63.js"
+  }
+]);
