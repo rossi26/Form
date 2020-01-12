b007dc639259e962ccac9b34ddea55b8a2875a76
Lorenzo <rossi.26.mario@gmail.com>
2020-01-12 16:02:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import Form from'./components/form/form'
+
+
+class App extends React.Component {
+  
+  render(){
+        return (
+        <div className="App">
+          <header className="App-header">
+            
+            <Form></Form>
+            
+          </header>
+        </div>
+      );
+    }
+  }
+
+export default App;
